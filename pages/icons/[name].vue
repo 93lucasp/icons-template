31c587f299d64c4c029d47a8490255ba9bb85875
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2023-03-06 09:38:53 +0100

    arrow button

--- pages/icons/[name].vue
+++ pages/icons/[name].vue
@@ -2,33 +2,37 @@
   
   <div class="bg-lightGray absolute w-full">
 
-
     <div class="bg-white">
-      <div class="pt-20 pb-28 max-w-md mx-auto text-center">
+      <div class="pt-20 pb-28 md:max-w-md max-w-xs mx-auto text-center ">
         <img :src="data.content.icon" alt="icon" class="mx-auto py-5 box-content">
-        <p class="font-sora pt-2 pb-3 font-semibold text-3xl">{{ data.content.title }}</p>
+        <p class="font-sora pt-2 pb-3 
+        font-semibold text-3xl">{{ data.content.title }}</p>
         <p class="text-darkBlue/50 text-sm ">{{ data.content.description }}</p>
       </div>
     </div>
       
-    <div class="columns-2 md:columns-3 lp:columns-5 lg:columns-4 px-14 relative bottom-10 gap-8">
-      <div v-for="image,index in data.content.gallery"  class="mb-8 border-8 border-white" @click="openModal(index)">
-          <nuxt-img provider="storyblok" loading="lazy" :src="image?.filename ?? '#'" alt="image" class="bg-white w-full border border-innerShadow" />
+    <div class="columns-2 md:columns-3 lp:columns-5 lg:columns-4 lg:px-14 px-3 relative bottom-10 lg:gap-8 gap-3">
+      <div v-for="image,index in data.content.gallery"  class="lg:mb-8 mb-3 border-8 border-white hover:cursor-pointer" @click="openModal(index)">
+        <nuxt-img preload provider="storyblok" :format="image?.filename.slice(-3) == 'svg' ? '' : 'webp' " loading="lazy" :src="image?.filename ?? '#'" alt="image" class="bg-white w-full border border-innerShadow" />
       </div>
     </div>
 
   </div>
 
   <div v-show="showModal" class="w-full h-full fixed flex items-center bg-darkBlue/50" @click="closeModal()">
-    <div class="flex w-full" @click.stop="">
-      <button class="bg-white" @click="previousImage()">Immagine Precendente</button>
-      <div class="max-w-3xl w-full relative mx-auto border-15 border-white">
-        <nuxt-img provider="storyblok" loading="lazy" :src="data.content.gallery[selectedImage]?.filename ?? '#'" alt="image" class="w-full border border-innerShadow bg-white" />
+    <div class="flex w-full justify-center max-h-screen">
+      <button @click.stop="previousImage()" class="lg:block hidden">
+        <img src="~/assets/img/left-arrow.svg" alt="left arrow icon" class="w-16 bg-white rounded-full shadow relative left-8 select-none" :class="{'opacity-50 cursor-not-allowed': selectedImage==0}">
+      </button>
+      <div class="max-w-5xl h-full lg:border-15 border-5 border-white" @click.stop="">
+        <img :src="data.content.gallery[selectedImage]?.filename ?? '#'" alt="image" class="h-full border border-innerShadow bg-white" />
       </div>
-      <button class="bg-white" @click="nextImage()">Immagine Successiva</button>
+      <button @click.stop="nextImage()" class="lg:block hidden">
+        <img src="~/assets/img/right-arrow.svg" alt="right arrow icon" class="w-16 bg-white rounded-full shadow relative right-8 select-none" :class="{'opacity-50 cursor-not-allowed': selectedImage==data.content.gallery.length - 1}" >
+      </button>
     </div>
   </div>
-
+  
 </template>
 
 <script setup>
@@ -53,23 +57,25 @@
       selectedImage.value--
   }
   function nextImage(){
-    if(selectedImage.value < data.value.content.gallery.length)
+    if(selectedImage.value < data.value.content.gallery.length - 1)
       selectedImage.value++
   }
 
-  document.onkeydown = (e) => {
-    e = e || window.event
-    if (e.key === "ArrowLeft") {
-      previousImage()
-    } 
-    else if (e.key === "ArrowRight") {
-      nextImage()
-    }
-    else if(e.key === "Escape"){
-      closeModal()
-    }
-  };
 
+  onMounted(() => {
+    document.onkeydown = (e) => {
+      e = e || window.event
+      if (e.key === "ArrowLeft") {
+        previousImage()
+      } 
+      else if (e.key === "ArrowRight") {
+        nextImage()
+      }
+      else if(e.key === "Escape"){
+        closeModal()
+      }
+    };
+  })
 
 
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -21,20 +21,8 @@
     </nuxt-link> 
 </div>
 
-<footer class="max-w-6xl mx-auto flex pb-8 pt-7 border-t border-darkBlue/10 text-xs text-darkBlue/50 justify-between">
-    <div class="space-x-7">
-        <nuxt-link class="font-semibold text-darkBlue">
-            2022 CallToInspiration · Tutti i diriti riservati
-        </nuxt-link>
-        <nuxt-link class="font-medium">About CTI</nuxt-link>
-        <a class="font-medium">Privacy</a>
-    </div>
-    <div class="flex">
-        <p>by</p>
-        <img src="#" alt="profile picture">
-        <p class="font-medium">Alexis Hill</p>
-    </div>
-</footer>
+
+<Footer />
 
 </template>
   
@@ -46,41 +34,7 @@
 
   const { data: icons } = await useFetch(`${baseUrl}?starts_with=icons${version+token}&per_page=100`)
   const index = await useAsyncStoryblok('index', { version: 'published' })
-//   const prova = await useStoryblok('icons/', { version: 'published' , starts_with: 'icons/' })
-//   console.log(prova);
-//   const story = await useAsyncStoryblok('home', { version: 'published' })
 
-//   const home = await useStoryblok("index", { version: "published" });
-//   const icons = await useStoryblok("", { "version": "published", "starts_with": "icons/" , "cv": 1676039583 });
-//   const { data: icons2 } = await useFetch('/api/count')
-
-
-//   const stories = ref(null);
-
-//   const storyblokApi = useStoryblokApi();
-  
-
-//   const { data } = await useAsyncData(
-//     "vue",
-//     async () =>
-//       await storyblokApi.get(`cdn/stories/`, {
-//         "version": "published",
-//         "starts_with": "icons/",
-//       })
-//   );
-
-
-//   const {data} = await useAsyncData(
-//     "vue",
-//     async () =>
-//       await storyblokApi.get(`cdn/stories/index`, {
-//         "version": "published",
-//       })
-//   );
-//   console.log(otherData.data);
-
-  
-//   stories.value = data.value.data.stories;
 
   </script>
 
